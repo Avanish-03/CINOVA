9b6ea55368cefd63032177fc6c4154faae6c0a47
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2024-06-29 10:01:46 +0530

    first commit

---
+++ dist/header.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="" href="../img/website-icon-img.png">
+    <title>Cinova</title>
+    <link rel="stylesheet" href="./output.css">
+    <!-- <script src="https://cdn.tailwindcss.com"></script> -->
+</head>
+
+<body class="bg-black">
+    <header class="text-gray-100 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+                <img src="../img/website-icon-img.png" alt="" class="h-8 p-1 rounded">
+                <span class="ml-3 text-xl text-white">CINOVA</span>
+            </a>
+            <nav class="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center">
+                <a class="mr-5 hover:text-gray-100 cursor-pointer">First Link</a>
+                <a class="mr-5 hover:text-gray-100 cursor-pointer">Second Link</a>
+                <a class="mr-5 hover:text-gray-100 cursor-pointer">Third Link</a>
+                <a class="mr-5 hover:text-gray-100 cursor-pointer">Fourth Link</a>
+            </nav>
+            <button class="inline-flex items-center bg-gray-900 border-0 py-1 px-3 focus:outline-none hover:bg-gray-800 rounded text-base mt-4 md:mt-0">Button
+    
+            </button>
+        </div>
+    </header>
+</body>
+
+</html>
